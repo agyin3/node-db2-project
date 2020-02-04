dffe63af01ec212abb76b46e1bb85a3fe949db75
Buddy Agyin <agyin3@gmail.com>
2020-02-03 23:18:54 -0700

    add middleware to server and link to router

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const helmet = require('helmet')
 const server = express()
 
-server.use(express.json)
+const carsRouter = require('./cars/carsRouter.js')
+
+server.use(helmet())
+server.use(logger)
+
+server.use(express.json())
+
+server.use('/api/cars', carsRouter)
+
+function logger (req, res, next) {
+    const date = new Date().toISOString()
+    console.log(`${req.method} request made at ${date} to ${req.url} from ${req.hostname}`)
+    next()
+}
 
 
 module.exports = server
